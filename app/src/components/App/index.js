b3cea7dfe3b4032adce72786d86d7f6066129abf
Philippe Candille <philippe@oclock.io>
2018-04-24 12:32:06 +0200

    Composant App

---
+++ app/src/components/App/index.js
@@ -0,0 +1,20 @@
+/*
+ * NPM import
+ */
+import React from 'react';
+
+/*
+ * Local import
+ */
+
+/*
+ * Code
+ */
+const App = () => (
+  <div id="app">Hello World</div>
+);
+
+/*
+ * Export
+ */
+export default App;
--- app/src/index.js
+++ app/src/index.js
@@ -1,3 +1,27 @@
-console.log('Coucou');
+/*
+ * NPM import
+ */
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
 
-const sum = (a, b) => a + b;
+/*
+ * Local import
+ */
+import App from 'src/components/App';
+
+/*
+ * Code
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  // Composant racine
+  const rootComponent = <App />;
+  // Noeud du DOM
+  const node = document.getElementById('root');
+  // Rendu
+  render(rootComponent, node);
+});
+
+/*
+ * Export
+ */
